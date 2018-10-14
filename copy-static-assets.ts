67a351005066b581bd469f627e65326d8afdc7b3
bjvickers <bjvickers@protonmail.com>
2018-10-14 09:50:10 +0000

    Moving towards Typescript

---
+++ copy-static-assets.ts
@@ -0,0 +1,5 @@
+import * as shell from "shelljs"
+
+shell.cp("-R", "src/public", "dist/public/")
+shell.cp("-R", "src/public/stylesheets", "dist/public/")
+shell.cp("-R", "src/views", "dist/views/")
